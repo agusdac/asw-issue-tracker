94458a96cf81b46320467e0599fc0abf44b1f8bf
agusdac <agusdac1@gmail.com>
2019-03-27 17:12:23 +0000

    enumeration para priority, fk users no funciona :(

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,2 +1,12 @@
 class Issue < ApplicationRecord
+    ALL_PRIORITIES = ["trivial", "minor", "major", "critical", "blocker"]
+    
+    validates_inclusion_of :priority, :in => ALL_PRIORITIES
+    
+    
+   #ALL_TYPES = ["bug", "enhancement", "proposal", "task"]
+    
+    #validates_inclusion_of :typeOfIssue, :in => ALL_TYPES  
+    
+    #belongs_to :user
 end
