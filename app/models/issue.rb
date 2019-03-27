71431499bec9e03d52850481f931f143cfb2aaf1
agusdac <agusdac1@gmail.com>
2019-03-27 22:02:36 +0000

    type fixed (ahora kind) con enumeration

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,12 +1,13 @@
 class Issue < ApplicationRecord
+    
     ALL_PRIORITIES = ["trivial", "minor", "major", "critical", "blocker"]
     
     validates_inclusion_of :priority, :in => ALL_PRIORITIES
     
     
-   #ALL_TYPES = ["bug", "enhancement", "proposal", "task"]
+    ALL_TYPES = ["bug", "enhancement", "proposal", "task"]
     
-    #validates_inclusion_of :typeOfIssue, :in => ALL_TYPES  
+    validates_inclusion_of :kind, :in => ALL_TYPES  
     
-    #belongs_to :user
+    #belongs_to :user, foreign_key: 'assignee'
 end
--- db/migrate/20190325224833_create_issues.rb
+++ db/migrate/20190325224833_create_issues.rb
@@ -2,7 +2,7 @@ class CreateIssues < ActiveRecord::Migration[5.1]
   def change
     create_table :issues do |t|
       t.string :title
-      t.string :type
+      t.string :kind
       t.string :priority
       t.string :status
       t.string :assignee
