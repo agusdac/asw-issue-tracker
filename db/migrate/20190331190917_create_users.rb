c3aa07eda97e3a24281db5a3554e0d1749ea45e4
RobertoUroz <roberto.uroz@est.fib.upc.edu>
2019-03-31 21:20:22 +0200

    Funciona GoogleSignIn en portatil localhost

---
+++ db/migrate/20190331190917_create_users.rb
@@ -0,0 +1,17 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    drop_table :users
+    create_table :users do |t|
+      t.string :provider
+      t.string :uid
+      t.string :name
+      t.string :email
+      t.string :created_at
+      t.string :updated_at
+      t.string :oauth_token
+      t.datetime :oauth_expires_at
+
+      t.timestamps
+    end
+  end
+end
