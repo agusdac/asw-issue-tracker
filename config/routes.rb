adff386a1145e784fde314836e0a4f3851fbd9da
RobertoUroz <roberto.uroz@est.fib.upc.edu>
2019-04-04 15:34:05 +0200

    AHora el root son issues

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   resource :home, only: [:show]
   resources :issues
   resources :users
-  root to: "home#show"
+  root to: "issues#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190331190917) do
+ActiveRecord::Schema.define(version: 20190404131303) do
 
   create_table "issues", force: :cascade do |t|
     t.string "title"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20190331190917) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "asignee_id"
+    t.index ["asignee_id"], name: "index_issues_on_asignee_id"
   end
 
   create_table "users", force: :cascade do |t|
