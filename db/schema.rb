9b3a346e447eae0f848aee0dac394e71d0870b56
EC2 Default User <ec2-user@ip-172-31-44-185.us-east-2.compute.internal>
2019-04-05 16:16:55 +0000

    first commit

--- db/schema.rb
+++ db/schema.rb
@@ -32,8 +32,6 @@ ActiveRecord::Schema.define(version: 20190404174931) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "asignee_id"
-    t.index ["asignee_id"], name: "index_issues_on_asignee_id"
   end
 
   create_table "users", force: :cascade do |t|
