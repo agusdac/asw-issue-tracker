f572450c9d0ca6204ae9b2c883aecc0da4029039
agusdac <agusdac1@gmail.com>
2019-04-08 21:36:21 +0000

    5th

--- db/migrate/20190404131303_add_fk_to_issues.rb
+++ db/migrate/20190404131303_add_fk_to_issues.rb
@@ -1,5 +1,6 @@
 class AddFkToIssues < ActiveRecord::Migration[5.1]
   def change
     add_reference :issues, :user, index: true
+    add_reference :issues, :assignee, class_name => 'User', index: true
   end
 end
