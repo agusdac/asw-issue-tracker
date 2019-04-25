2eb1d005ad648f0113db23f38457dc2f75dee71e
Raul Mateo Beneyto <raulmateob@gmail.com>
2019-04-25 14:56:15 +0200

    second test

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,7 +6,7 @@ class CommentsController < ApplicationController
     # GET /comments
     # GET /comments.json
     def index
-        @comments = Comments.all
+        @comment = Comments.all
     end
 
     def create
