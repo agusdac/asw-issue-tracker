3c4fb85210e0abd80198e37082612d3ecbd929d1
RobertoUroz <roberto.uroz@est.fib.upc.edu>
2019-04-25 14:28:18 +0200

    CORS v.2

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module HelloApp
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get :post :put :patch :delete :options :head]
+        resource '*', headers: :any, methods: [get post put patch delete options head]
       end
     end
   end
